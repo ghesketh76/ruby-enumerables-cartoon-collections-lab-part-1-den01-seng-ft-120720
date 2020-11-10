ade69f177c8d2caa7cf0be291e62f2568af60790
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 18:29:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element 
   
   array.each_with_index do |item, index|
-    puts index + ". " + item 
+    puts index.to_s + ". " + item 
   end
   
 end
